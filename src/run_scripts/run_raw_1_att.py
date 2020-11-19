15e5dc4e44b5f4a84228067b1435bde5b4fe1882
Lucas Fenaux <46611744+LucasFenaux@users.noreply.github.com>
2020-11-19 14:56:06 -0500

    fixing typos in import

--- src/run_scripts/run_raw_1_att.py
+++ src/run_scripts/run_raw_1_att.py
@@ -1,12 +1,12 @@
 from __future__ import absolute_import
 import numpy as np
 import sys
+import os
 sys.path.insert(0, '/home/fenauxlu/projects/rrg-mdiamond/fenauxlu/ScdmsML/src')
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
 
-import os
 import torch
 import torch.optim as optim
 import logging
