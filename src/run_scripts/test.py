5282fe6b5d362a2d9720d1bdc1db9cdc7ba8f29f
Lucas Fenaux <lu.fenaux@gmail.com>
2020-12-29 15:35:26 -0500

    adding some test code for V1.0 of Lstm

--- src/run_scripts/test.py
+++ src/run_scripts/test.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import
-import numpy as np
 import sys
 import os
 sys.path.insert(0, '/home/fenauxlu/projects/rrg-mdiamond/fenauxlu/ScdmsML/src/')
@@ -7,10 +6,84 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
 
-from src.utils.data_loading import torch_raw_data_loader
+#Importing Libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from src.models.model import LSTMClassifier
+import torch
+import torch.optim as optim
+from ignite.engine import Events, create_supervised_trainer, create_supervised_evaluator
+from torch.utils.data import TensorDataset, RandomSampler, DataLoader
+
 
 if __name__ == '__main__':
-        train_loader, test_loader = torch_raw_data_loader(batch_size=200, num_workers=1)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    train = pd.read_csv('../input/train_1.csv').fillna(0)
+    page = train['Page']
+    train.head()
+    # Dropping Page Column
+    train = train.drop('Page', axis=1)
+    # Using Data From Random Row for Training and Testing
 
-        for x in train_loader:
-            print(x[0].size())
+    row = train.iloc[90000, :].values
+    X = row[0:549]
+    y = row[1:550]
+    # Splitting the dataset into the Training set and Test set
+    from sklearn.model_selection import train_test_split
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+    # Feature Scaling
+    from sklearn.preprocessing import MinMaxScaler
+
+    sc = MinMaxScaler()
+    X_train = np.reshape(X_train, (-1, 1))
+    y_train = np.reshape(y_train, (-1, 1))
+    X_train = sc.fit_transform(X_train)
+    y_train = sc.fit_transform(y_train)
+
+    input_size = 1
+    hidden_size = 8
+    label_size = 1
+    batch_size = 10
+    epochs = 100
+
+    criterion = torch.nn.MSELoss()
+    regressor = LSTMClassifier(input_size, hidden_size, label_size, 1, 165)
+    regressor = regressor.to(device)
+    learning_rate = 0.01
+    optimizer = optim.Adam(regressor.parameters(), lr=learning_rate)
+
+    trainer = create_supervised_trainer(regressor, optimizer, criterion, device=device)
+
+    # Setting up the dataset
+    train_data = torch.Tensor(X_train)
+    train_targets = torch.Tensor(y_train)
+
+    train_dataset = TensorDataset(train_data, train_targets)
+    train_sampler = RandomSampler(train_dataset)
+    train_loader = DataLoader(train_dataset, sampler=train_sampler, batch_size=batch_size, num_workers=1,
+                              pin_memory=True)
+
+    trainer.run(train_loader, max_epochs=epochs)
+
+    # Getting the predicted Web View
+    inputs = X_test
+    inputs = np.reshape(inputs, (-1, 1))
+    inputs = sc.transform(inputs)
+    inputs = np.reshape(inputs, (165, 1, 1))
+    y_pred = regressor(inputs).detach().to(torch.device('cpu')).numpy()
+    y_pred = sc.inverse_transform(y_pred)
+
+    # Visualising Result
+    plt.figure
+    plt.plot(y_test, color='red', label='Real Web View')
+    plt.plot(y_pred, color='blue', label='Predicted Web View')
+    plt.title('Web View Forecasting')
+    plt.xlabel('Number of Days from Start')
+    plt.ylabel('Web View')
+    plt.legend()
+    plt.show()
+    plt.savefig('./out_fig.png')
+
